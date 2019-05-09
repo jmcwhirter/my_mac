9f56ac5087d3615e9533467cb4c1e07a19437068
Justin McWhirter <justin.mcwhirter@gmail.com>
2019-05-09 14:19:51 -0500

    Added OhMyZSH installer and coonfig. Added readme.

---
+++ README.md
@@ -0,0 +1,2 @@
+sh -c "$(curl -fsSL https://github.com/jmcwhirter/my_mac/brew.sh)"
+sh -c "$(curl -fsSL https://github.com/jmcwhirter/my_mac/zsh.sh)"
---
+++ zsh.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+echo 'alias ll="ls -la"
+
+eval $(thefuck --alias)
+
+export GOPATH=$HOME/golang
+export GOROOT=/usr/local/opt/go/libexec
+export PATH=$PATH:$GOPATH/bin
+export PATH=$PATH:$GOROOT/binjustin' >> ~/.zshrc
